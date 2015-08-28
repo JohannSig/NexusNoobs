c849c9146c6c6fcc8343691df2fa25b1eb457acd
johannsig <johannsig@gmail.com>
2015-08-28 15:36:35 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">  
+        <meta name="description" content="Nexus Noobs borðspil bíó og áhugamál fyrir börn og unglinga" />
+        <title>NΞXU5 N00B5</title>
+        <link rel="StyleSheet" href="content/css/bootstrap.min.css" type="text/css" media="screen" >        
+        <link rel="StyleSheet" href="content/css/project.css" type="text/css" media="screen" >
+        <script type="text/javascript" src="content/scripts/jquery-2.1.1.min.js"></script> 
+        <script type="text/javascript" src="content/js/bootstrap.min.js"></script>
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300' rel='stylesheet' type='text/css'>
+        <base target="_blank" />
+    </head>
+
+    <body>
+
+        <script>
+            (function(i, s, o, g, r, a, m) {
+                i['GoogleAnalyticsObject'] = r;
+                i[r] = i[r] || function() {
+                    (i[r].q = i[r].q || []).push(arguments)
+                }, i[r].l = 1 * new Date();
+                a = s.createElement(o),
+                        m = s.getElementsByTagName(o)[0];
+                a.async = 1;
+                a.src = g;
+                m.parentNode.insertBefore(a, m)
+            })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+            ga('create', 'UA-66659048-1', 'auto');
+            ga('send', 'pageview');
+        </script>
+
+        <div id="header">
+            <div class="container">
+                <div>
+                    <a href="http://nexus.is/vefverslun/"                        
+                       title="Nexus"><img style="margin-top: 13px; height: 85px;" 
+                                       src="http://nexus.is/vefverslun/img/logo.png?1438082978" 
+                                       alt="Nexus" > &nbsp; <img style="margin-top: -10px; height: 68px;" src="content/images/noobs.png"  /></a>                    
+                    <span style="float: right; margin-top: 32px;">
+                        <a href="https://www.facebook.com/NexusNoobs" ><img title="Nexus Noobs á Facebook" src="content/images/facebook.png" style="height: 42px;" /></a>
+                        <a href="http://instagram.com/nexusnoobs?ref=badge" ><img title="Nexus Noobs á Instagram" src="content/images/instagram-wide.png" style="height: 48px;" /></a>
+                        <a href="mailto:nexusnoobs@gmail.com" ><img title="Nexus Noobs á GMail" src="content/images/gmail.png" style="height: 42px;" /></a>
+                    </span>
+                </div>
+
+            </div>
+        </div>
+
+
+        <div class="container">
+
+            <div class="page-top"></div>
+
+            <div class="page-content">
+                <div id="jumbo">
+                    <p class="clearfix"><img src="content/images/darthvader.png" style="width: 100px; float: right;" />Nexus Noobs námskeiðin eru ætluð ungu fólki á aldrinum 12 til 20 ára sem langar til þess að læra inn á ný áhugamál eða að læra meira um viðfangsefnin sem þau þegar þekkja.</p>                    
+                    <p class="clearfix"><img src="content/images/dice.png" style="width: 120px; float: right;" />Á námskeiðinu gefst þátttakendum tækifæri til þess að kynnast frábærum félögum sem deila áhuganum og tengjast því fjölbreytta félagsstarfi sem á sér stað í Nexus.</p>                    
+                    <p class="clearfix"><img src="content/images/warhammer.png" style="width: 130px; float: right;" />Hittingum er stjórnað af færustu sérfræðingum Nexus og læra þátttakendur inn á borðspil, safnkortaspil, hlutverkaspil, módelsmíði, herkænskuleiki, vísindaskáldskap, fantasíur, myndasögur og margt fleira.</p>
+                </div>                
+                <div class="row">
+
+                    <!-- Fyrsti dálkur -->
+                    <div class="col-xs-12 col-sm-6 col-md-4" style="margin-bottom: 10px;">
+                        <div class="column red">
+                            <div class="header">Næstu námskeið</div>
+
+                            <div>
+                                <h3>Haustnámskeið 2015<br/>15. september</h3>
+                                <p>Unglingar 12 til 20 ára.<br/>
+                                    10 skipti <br/>
+                                    kr. 22.000,-*</p>
+                                <p>Á námskeiðinu fá unglingarnir tækifæri til þess að fræðast um ný áhugamál í hverri viku undir handleiðslu færustu sérfræðinga Nexus.</p>
+                                <p>Hittingarnir fara flestir fram á þriðjudögum í spilasal Nexus stundvíslega milli kl 17 og 19.</p>
+                                <p><em>*Hægt er að nýta frístundarkortið til niðurgreiðslu.</em></p>
+                                <p><a href="content/docs/nexus-noobs-haustnamskeid-form.docx">Umsóknareyðublað</a><br/><a href="content/docs/nexus-noobs-upplysingar-til-foreldra-haust-2015.pdf">Hagnýtar upplýsingar</a></p>
+                                <p><a href="mailto:nexusnoobs@gmail.com" class="btn btn-danger btn-lg">Skrá á námskeið</a></p>                                
+                            </div>   
+
+                            <div>
+                                <h3>D&D Hlutverkaspil<br/>Október 2015</h3>
+                                <p>Námskeið í hlutverkaspilinu Dungeons and Dragons.</p>
+                                <p><em>Frekari upplýsingar á næstu dögum.</em></p>  
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Miðjudálkur -->
+                    <div class="col-xs-12 col-sm-6 col-md-4" style="margin-bottom: 10px;">
+                        <div class="column red">
+                            <div class="header">Taktu þátt</div>
+                            <p>Námskeiðin eru ætluð öllum stelpum og strákum sem hafa áhuga á að kynnast nýjum áhugamálum og félögum.</p>
+                            <p>Fyrri þekking á viðfangsefnum skiptir engu máli og er þátttakendum gjarnan skipt upp í smærri hópa og róterað.</p>
+
+                            <h3>Reglur Nexus Noobs</h3>
+                            <ol>
+                                <li>Við komum vel fram við hvort annað og erum jafningjar</li>
+                                <li>Við virðum ólík áhugamál félaga okkar</li>
+                            </ol>
+
+                            <p>Tryggja má pláss fyrir þátttakanda á námskeiðunum með því að millifæra upphæðina inn á reikning nr. 0318-26-7778 kt. 490315-2090 (Sentia) og skrá nafn barns í greiðsluskýringu.</p>
+                            <p>Leyfilegt er að sitja sömu námskeiðin oftar en einu sinni og aðstoðum við ykkur einnig við að komast inn í frekara félagsstarf sem Nexus stendur fyrir.</p>
+                            <p>Fyrir frekari upplýsingar er hægt að hafa samband við okkur með <a href="mailto:nexusnoobs@gmail.com"">tölvupósti</a> eða í skilaboðum á <a href="https://www.facebook.com/Nexusnoobs"">facebooksíðu</a> Nexus Noobs.</p>
+
+                            <p><a href="mailto:nexusnoobs@gmail.com" class="btn btn-danger btn-lg">Sendu okkur póst</a></p>
+                        </div>
+                    </div>
+
+
+                    <!-- Þriðji dálkur -->
+                    <div class="col-xs-12 col-sm-6 col-md-4" style="margin-bottom: 10px;">
+                        <div class="column red">
+                            <div class="header">Hópastarfið</div>
+                            <p>Nexus Noobs er stjórnað af ólíkum aðilum sem eiga það sameiginlegt að aðhyllast hin ýmsu geek áhugamál.</p>
+
+                            <p>Hittingum er vikulega stjórnað af ýmsum aðilum sem hverjir eru sérfróðir um viðfangsefnið sem tekið er fyrir.</p>
+                            <p>Á öllum hittingum eru þau Elín Anna Baldursdóttir og/eða Ágúst Þorvaldsson til staðar og aðstoða bæði stjórnendur og þátttakendur á námskeiðum.</p>
+                            <p>Ábyrgðaraðili Nexus Noobs er <a href="http://www.soffiaelin.is"">Soffía&nbsp;Elín&nbsp;Sigurðardóttir</a></p>
+                            <div class="text-center">
+
+                            </div>
+                        </div>
+                    </div>            
+
+                </div>
+            </div>
+
+
+            <img src="content/images/marvel.png" style="width: 100%; box-shadow: 1px 10px 15px -4px rgba(0,0,0,0.5);"/>
+
+            <!--
+            <div id="footer" class="text-center">  
+
+            </div>
+            -->
+        </div>  
+
+        <div class="text-center" style="margin-top: 5px; "><small>Vefútfærsla: Kaldasmíð ehf, Reykjavík 2015</small></div>
+
+    </div>
+
+
+
+
+</body>
+</html>
